ff7d60f60dea4699f5c0c51ae6c493562fb79b3c
Daniel <danwater1@gmail.com>
2018-03-04 23:33:38 -0800

    Finished redux isolation

---
+++ src/playground/destructuring.js
@@ -0,0 +1,39 @@
+// const person = {
+//   name: 'Daniel',
+//   age: 37,
+//   location: {
+//     city: 'San Jose',
+//     temp: 42
+//   }
+// };
+
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
+
+// const { city, temp: temperature } = person.location;
+
+// if (city && typeof temperature === 'number') {
+//   console.log(`It's ${temperature} in ${city}.`);
+// }
+
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// }
+
+// const { name: publisherName = 'Self Published' } = book.publisher;
+// console.log(publisherName);
+
+// Array destructuring
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+const [ , city , state = 'California' ] = address;
+
+console.log(`You are in ${state}. `)
+
+const item = ['Iced Coffee (hot)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice, ] = item;
+console.log(`A ${itemName} costs ${mediumPrice}`);
