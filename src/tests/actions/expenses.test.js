92e082b36c3053a30b2b652a3c8de927c2de9778
Daniel Waters <danwater1@gmail.com>
2018-03-13 10:41:25 -0700

    Added update firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   startAddExpense,
   setExpenses,
   startSetExpenses,
-  startRemoveExpense } from '../../actions/expenses.js';
+  startRemoveExpense,
+  startEditExpense } from '../../actions/expenses.js';
 import expenses from '../fixtures/expenses';
 import  database  from '../../firebase/firebase';
 
@@ -53,6 +54,24 @@ test('should setup edit expense action object', () => {
     updates: { 
       description: 'Some description' 
     }
+  });
+});
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[1].id;
+  const updates = { amount: 21045 };
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
   });
 });
 
