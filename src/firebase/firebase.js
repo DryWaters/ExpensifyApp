fd682f441574c603592123360e071b10267b9b5c
Daniel <danwater1@gmail.com>
2018-03-12 22:28:50 -0700

    Added test database and environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import moment from 'moment';
 
 const config = {
-  apiKey: "AIzaSyDrtjhlzGe8p2EC6wiY4IWsncLlbRFL3dg",
-  authDomain: "expensify-5b7e9.firebaseapp.com",
-  databaseURL: "https://expensify-5b7e9.firebaseio.com",
-  projectId: "expensify-5b7e9",
-  storageBucket: "expensify-5b7e9.appspot.com",
-  messagingSenderId: "993430400439"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
